fb487518ecd8071e80f3dc69f404b50dff1ea6eb
Jakub Góźdź <jg@redbyte.pl>
2022-09-23 13:24:33 +0200

    Step 1
    * Basic object types added
    * Textures, sounds, and music initialization added

--- core/src/pl/redbyte/jumptutorial/JumpTutorial.java
+++ core/src/pl/redbyte/jumptutorial/JumpTutorial.java
@@ -1,17 +1,46 @@
 package pl.redbyte.jumptutorial;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class JumpTutorial extends ApplicationAdapter {
-	SpriteBatch batch;
+
+	
+	private Music music;
+	private Texture playerTexture, platformTexture;
+	private JumpPlayer player;
+	private Array<Platform> platforms;
+	private OrthographicCamera camera;
+	private float gravity = -20;
+	private SpriteBatch batch;
+
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		loadData();
+		init();
+
 	}
+
+
+	private void init() {
+		batch = new SpriteBatch();
+		music.play();
+	}
+
+
+	private void loadData() {
+		playerTexture = new Texture("player.png");
+		platformTexture = new Texture("platform.png");
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.ogg"));
+	}
+
 
 	@Override
 	public void render () {
@@ -19,7 +48,8 @@ public class JumpTutorial extends ApplicationAdapter {
 		batch.begin();
 		batch.end();
 	}
-	
+
+
 	@Override
 	public void dispose () {
 		batch.dispose();
